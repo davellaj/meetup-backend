5ad6068dd83447c9c1d44788568dd431d0325422
davellaj <jamie.davella@gmail.com>
2017-06-23 13:20:05 -0400

    set up backend for meetup and group routes and db

---
+++ server/modules/groups/controller.js
@@ -0,0 +1,64 @@
+import Group from './model';
+
+export const createGroup = async (req, res) => {
+  const {
+    name,
+    description,
+    category,
+  } = req.body;
+
+  if (!name) {
+    res.status(400).json({ error: true, message: 'Name must be provided' });
+  } else if (typeof name !== 'string') {
+    res.status(400).json({ error: true, message: 'Name must be a string' });
+  } else if (name.length < 5) {
+    res.status(400).json({ error: true, message: 'Name must be at least 5 characters long' });
+  }
+
+  if (!description) {
+    res.status(400).json({ error: true, message: 'Description must be provided' });
+  } else if (typeof description !== 'string') {
+    res.status(400).json({ error: true, message: 'Description must be a string' });
+  } else if (description.length < 10) {
+    res.status(400).json({ error: true, message: 'Description must be at least 10 characters long' });
+  }
+
+  const group = new Group({ name, description });
+  try {
+    return res.status(201).json({ error: false, group: await group.save() });
+  } catch (e) {
+    res.status(400).json({ error: true, message: 'error when group was created' });
+  }
+};
+
+export const createGroupMeetup = async (req, res) => {
+  const { title, description } = req.body;
+  const { groupId } = req.params;
+
+  if (!title) {
+    res.status(400).json({ error: true, message: 'Title must be provided' });
+  } else if (typeof title !== 'string') {
+    res.status(400).json({ error: true, message: 'Title must be a string' });
+  } else if (title.length < 5) {
+    res.status(400).json({ error: true, message: 'Title must be at least 5 characters long' });
+  }
+
+  if (!description) {
+    res.status(400).json({ error: true, message: 'Description must be provided' });
+  } else if (typeof description !== 'string') {
+    res.status(400).json({ error: true, message: 'Description must be a string' });
+  } else if (description.length < 10) {
+    res.status(400).json({ error: true, message: 'Description must be at least 10 characters long' });
+  }
+
+  if (!groupId) {
+    res.status(400).json({ error: true, message: 'GroupId must be provided' });
+  }
+
+  try {
+    const [meetup, group] = await Group.addMeetup(groupId, { title, description });
+    return res.status(201).json({ error: false, meetup, group });
+  } catch (e) {
+    res.status(400).json({ error: true, message: 'Meetup could not be created' });
+  }
+};
